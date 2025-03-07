21d3ff7cf324032b54f0f9ada0e241a3a8fc30f1
Max Anurin <maxim.anurin@zxteam.org>
2025-03-07 20:48:58 +0200

    Rename FSleep -> fsleep

--- src/lifecycle/f_sleep.ts
+++ src/lifecycle/f_sleep.ts
@@ -2,27 +2,36 @@ import { FCancellationExecutionContext, FCancellationException, FCancellationTok
 import { FExecutionContext } from "../execution_context/index.js";
 
 /**
- * Provide a "sleeping" `Promise` that completes via timeout or cancellationToken
- * @param cancellationToken The cancellation token to cancel "sleeping"
- * @param ms Timeout delay in milliseconds. If ommited, the "sleeping" `Promise` will sleep infinitely and wait for cancellation token activation
- * @example
- * await FSleep(FCancellationToken.Dummy, 25); // Suspend execution for 25 milliseconds
- * @example
- * const cancellationTokenSource = new FCancellationTokenSourceManual();
- * ...
- * await FSleep(cancellationTokenSource.token, 25); // Suspend execution for 25 milliseconds or cancel if cancellationTokenSource activates
- * @example
- * const cancellationTokenSource = new FCancellationTokenSourceManual();
- * ...
- * await FSleep(cancellationTokenSource.token); // Suspend infinitely while cancellationTokenSource activates
- * @example
- * const executionContext: FExecutionContext = ...;
- * ...
- * await FSleep(executionContext); // Cancellation token will extracted from execution context
+ * @deprecated Use `fsleep()` instead
  */
 export function FSleep(cancellationToken: FCancellationToken, ms?: number): Promise<void>;
 export function FSleep(executionContext: FExecutionContext, ms?: number): Promise<void>;
 export function FSleep(data: FExecutionContext | FCancellationToken, ms?: number): Promise<void> {
+	return fsleep(data as any, ms);
+}
+
+/**
+ * Provide a "sleeping" `Promise` that completes via timeout or cancellationToken
+ * @param cancellationToken The cancellation token to cancel "sleeping"
+ * @param ms Timeout delay in milliseconds. If omitted, the "sleeping" `Promise` will sleep infinitely and wait for cancellation token activation
+ * @example
+ * await fsleep(FCancellationToken.Dummy, 25); // Suspend execution for 25 milliseconds
+ * @example
+ * const cancellationTokenSource = new FCancellationTokenSourceManual();
+ * ...
+ * await fsleep(cancellationTokenSource.token, 25); // Suspend execution for 25 milliseconds or cancel if cancellationTokenSource activates
+ * @example
+ * const cancellationTokenSource = new FCancellationTokenSourceManual();
+ * ...
+ * await fsleep(cancellationTokenSource.token); // Suspend infinitely while cancellationTokenSource activates
+ * @example
+ * const executionContext: FExecutionContext = ...;
+ * ...
+ * await fsleep(executionContext); // Cancellation token will extracted from execution context
+ */
+export function fsleep(cancellationToken: FCancellationToken, ms?: number): Promise<void>;
+export function fsleep(executionContext: FExecutionContext, ms?: number): Promise<void>;
+export function fsleep(data: FExecutionContext | FCancellationToken, ms?: number): Promise<void> {
 	const cancellationToken: FCancellationToken = data instanceof FExecutionContext
 		? FCancellationExecutionContext.of(data).cancellationToken
 		: data;

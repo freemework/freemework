8ef83d4311479164f5995d1c1a66e54cd0f15756
Max Anurin <maxim.anurin@zxteam.org>
2025-05-03 14:26:37 +0300

    Fix flauncher export

--- src/launcher/index.ts
+++ src/launcher/index.ts
@@ -2,3 +2,4 @@ export { FLauncherException } from "./f_launcher_exception.js";
 export { FLauncherInitializeRuntimeException } from "./f_launcher_initialize_runtime_exception.js";
 export { FLauncherRestartRequiredException } from "./f_launcher_restart_required_exception.js";
 export type { FConfiglessRuntimeFactory, FLauncherRuntimeFactory } from "./f_launcher.js";
+export { flauncher } from "./f_launcher.js";
